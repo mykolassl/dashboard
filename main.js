b1eef2cf4cea7736d5bd9d80f3326cd1daf24083
mykolassl <emykolas@gmail.com>
2022-07-12 17:03:23 +0300

    Disable scroll when side menu is open on smaller screens

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('aside');
+const body = document.querySelector('body');
 
 burger.addEventListener('click', () => {
     if(burger.classList.contains('open')) {
         burger.classList.remove('open');
         menu.classList.remove('open');
+        body.style.position = 'static';
     } else {
         burger.classList.add('open');
         menu.classList.add('open');
+        body.style.position = 'fixed';
     }
 });
